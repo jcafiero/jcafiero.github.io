d38d7dbde10eec8fb4630b0d487aed9d2060fe2a
Jennifer Cafiero <jcafiero2@gmail.com>
2021-05-20 00:29:43 -0400

    Updates

---
+++ precache-manifest.54a044ba2a361c2d2c407c7cdc62f48c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69e4be0557a5c9be52bd",
+    "url": "/static/css/main.3e87a8f4.chunk.css"
+  },
+  {
+    "revision": "69e4be0557a5c9be52bd",
+    "url": "/static/js/main.7ef86c88.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99cbb30a08686fd64250",
+    "url": "/static/js/2.b72d0981.chunk.js"
+  },
+  {
+    "revision": "1d43354fd6bd52eae52a938195da8187",
+    "url": "/index.html"
+  }
+];
