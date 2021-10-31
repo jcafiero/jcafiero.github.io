c9a6890fc5c4869f200583380dc837c5b3ab9c68
Jennifer Cafiero <jcafiero2@gmail.com>
2021-10-31 16:52:10 -0400

    covert Error component to jsx

---
+++ src/components/ErrorNotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorNotFound = () => (
+	<div className="container">
+		<h2>I&apos;m sorry, but the page you are trying to reach cannot be found.</h2>
+	</div>
+);
+
+export default ErrorNotFound;
